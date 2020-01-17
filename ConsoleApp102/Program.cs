27b3cc0275e63ca72cf783dcdc36ecf8985669c5
Młyński Stanisław <mlynski.stanislaw@gmail.com>
2020-01-17 16:44:09 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp102/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp102
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program creates random numbers and saves them to file. ");
+            string nazwa = @"c:\tmp\totolotek.txt";
+
+            FileStream plik = new FileStream(nazwa, FileMode.Create, FileAccess.Write, FileShare.None);
+            System.IO.StreamWriter zapis = new StreamWriter(plik);
+            int [] tab = new int[6];
+            Random r = new Random();
+            for (int i=0; i<=5; i++)
+            {
+                tab[i] = r.Next(1, 49);
+                Console.WriteLine(tab[i]);
+                zapis.WriteLine(tab[i]);
+            }
+            zapis.Close();
+            plik.Close();
+            string b = @"c:\tmp\totolotek.txt";
+            FileStream p = new FileStream(b, FileMode.Open, FileAccess.Read, FileShare.None);
+            System.IO.StreamReader z = new StreamReader(p);
+            z.Close();
+            p.Close();
+            Console.ReadKey();
+        }
+    }
+}
